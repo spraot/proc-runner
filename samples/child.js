875de2d97b9932f413e0ef0005efd4c541e6ad45
rachetfoot <jason@boileau.dk>
2017-11-16 23:17:47 +0100

    Throttle display of process output

--- samples/child.js
+++ samples/child.js
@@ -4,19 +4,21 @@ const proc_length = parseInt(process.argv[2]);
 let progress = 0.0;
 
 const show_progress = () => {
-	progress += 0.1*(Math.random()*4+8);
+    if (progress >= proc_length)
+        process.exit();
 
 	console.log('T='+progress.toFixed(2)+'s Progress='+(progress/proc_length*100).toFixed(0)+'%');
 
-	if (Math.random()>0.98) {
+	if (Math.random()>0.9999) {
 		console.log('Some random error!');
 		process.exit(1);
 	}
 
-	if (progress >= proc_length)
-		process.exit();
-
-	setTimeout(show_progress, 100*(Math.random()*4+2));
+    const interval = 10*(Math.random()*4+1);
+	setTimeout(() => {
+        progress += interval/1000;
+	    show_progress();
+    }, interval);
 };
 
 show_progress();
