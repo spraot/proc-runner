54ec1b18d6d0bfeda04d311697de8f5883ba8ff9
rachetfoot <jason@boileau.dk>
2019-06-21 12:09:51 +0200

    Change sample file a bit

--- samples/simple.js
+++ samples/simple.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 
-const proc_count = 10;
+const proc_count = 1000;
 console.log(`Running ${proc_count} subprocesses...\n`);
 
 const runner = require('../lib/runner')({printStatus: true});
-for (const _ of new Array(proc_count))
+for (const i of [...Array(proc_count).keys()]) {
     runner.addProc({
         name: 'long running',
         exec: 'node',
-        args: ['-e', "console.log('started'); rem = Math.random()*20; setInterval(() => {(rem=rem-0.5)>0 || process.exit(); console.log('remaining: '+rem.toFixed(1)+'s')}, 500);"]
+        args: ['-e', "console.log('started'); rem = Math.random()*20; setInterval(() => {(rem=rem-0.5)>0 || console.log('done proc "+(i+1)+"') || process.exit(); console.log('Remaining: '+rem.toFixed(1)+'s')}, 500);"]
     });
+}
 
 runner.on('terminated', function(reason) {
     if (reason)
